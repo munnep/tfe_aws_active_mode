e2f8831b7f99ff9122ac9e4213c49c50a2e534ab
munnep <patrick.munne@hashicorp.com>
2022-09-20 15:18:15 +0200

    launch configuration

--- main.tf
+++ main.tf
@@ -492,54 +492,54 @@ resource "aws_db_instance" "default" {
 
 
 
-# resource "aws_launch_configuration" "as_conf" {
-#   name_prefix          = "${var.tag_prefix}-lc"
-#   image_id             = var.ami
-#   instance_type        = "t3.xlarge"
-#   security_groups      = [aws_security_group.tfe_server_sg.id]
-#   iam_instance_profile = aws_iam_instance_profile.profile.name
-#   key_name             = "${var.tag_prefix}-key"
+resource "aws_launch_configuration" "as_conf" {
+  name_prefix          = "${var.tag_prefix}-lc"
+  image_id             = var.ami
+  instance_type        = "t3.xlarge"
+  security_groups      = [aws_security_group.tfe_server_sg.id]
+  iam_instance_profile = aws_iam_instance_profile.profile.name
+  key_name             = "${var.tag_prefix}-key"
 
-#   root_block_device {
-#     volume_size = 50
+  root_block_device {
+    volume_size = 50
 
-#   }
+  }
 
-#   ebs_block_device {
-#     device_name = "/dev/sdh"
-#     volume_size = 32
-#     volume_type = "io1"
-#     iops        = 1000
-#   }
+  ebs_block_device {
+    device_name = "/dev/sdh"
+    volume_size = 32
+    volume_type = "io1"
+    iops        = 1000
+  }
 
-#   ebs_block_device {
-#     device_name = "/dev/sdi"
-#     volume_size = 100
-#     volume_type = "io1"
-#     iops        = 2000
-#   }
-
-
-#   user_data = templatefile("${path.module}/scripts/user-data.sh", {
-#     tag_prefix         = var.tag_prefix
-#     filename_airgap    = var.filename_airgap
-#     filename_license   = var.filename_license
-#     filename_bootstrap = var.filename_bootstrap
-#     dns_hostname       = var.dns_hostname
-#     tfe_password       = var.tfe_password
-#     dns_zonename       = var.dns_zonename
-#     pg_dbname          = aws_db_instance.default.name
-#     pg_address         = aws_db_instance.default.address
-#     rds_password       = var.rds_password
-#     tfe_bucket         = "${var.tag_prefix}-bucket"
-#     region             = var.region
-#   })
+  ebs_block_device {
+    device_name = "/dev/sdi"
+    volume_size = 100
+    volume_type = "io1"
+    iops        = 2000
+  }
 
 
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+  user_data = templatefile("${path.module}/scripts/user-data.sh", {
+    tag_prefix         = var.tag_prefix
+    filename_airgap    = var.filename_airgap
+    filename_license   = var.filename_license
+    filename_bootstrap = var.filename_bootstrap
+    dns_hostname       = var.dns_hostname
+    tfe_password       = var.tfe_password
+    dns_zonename       = var.dns_zonename
+    pg_dbname          = aws_db_instance.default.db_name
+    pg_address         = aws_db_instance.default.address
+    rds_password       = var.rds_password
+    tfe_bucket         = "${var.tag_prefix}-bucket"
+    region             = var.region
+  })
+
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 
 
 # # # # # Automatic Scaling group
