7317d19b4bccba6eced207c57a66aa5924b75201
munnep <patrick.munne@hashicorp.com>
2022-10-10 14:29:56 +0200

    changes to have an ative/active in on go

--- main.tf
+++ main.tf
@@ -1,3 +1,40 @@
+resource "random_id" "archivist_token" {
+  byte_length = 16
+}
+
+resource "random_id" "cookie_hash" {
+  byte_length = 16
+}
+
+resource "random_id" "enc_password" {
+  byte_length = 16
+}
+
+resource "random_id" "install_id" {
+  byte_length = 16
+}
+
+resource "random_id" "internal_api_token" {
+  byte_length = 16
+}
+
+resource "random_id" "root_secret" {
+  byte_length = 16
+}
+
+resource "random_id" "registry_session_secret_key" {
+  byte_length = 16
+}
+
+resource "random_id" "registry_session_encryption_key" {
+  byte_length = 16
+}
+
+resource "random_id" "user_token" {
+  byte_length = 16
+}
+
+
 resource "aws_vpc" "main" {
   cidr_block = var.vpc_cidr
 
--- output.tf
+++ output.tf
@@ -2,14 +2,6 @@ output "ssh_tf_client" {
   value = "ssh ubuntu@${var.dns_hostname}-client.${var.dns_zonename}"
 }
 
-output "tfe_dashboard" {
-  value = "https://${var.dns_hostname}.${var.dns_zonename}:8800"
-}
-
-output "tfe_netdata_performance_dashboard" {
-  value = "http://${var.dns_hostname}.${var.dns_zonename}:19999"
-}
-
 output "tfe_appplication" {
   value = "https://${var.dns_hostname}.${var.dns_zonename}"
 }
